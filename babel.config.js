883dcdfcf7700e6f3fc90047d251ed5028379624
wangchao <wcd126yx@126.com>
2019-08-20 15:34:04 +0800

    2222222222

--- babel.config.js
+++ babel.config.js
@@ -5,12 +5,9 @@ module.exports = {
 }
 
 let str = 'Creating a new branch is quick.'
-<<<<<<< HEAD
 // 还是有两个
 // 其实还是会有问题
 // 继续测试提交
 // 999999
-=======
 // 继续测试提交
 
->>>>>>> 6902e87c7ae090d003387543370c50338fd42bbb
